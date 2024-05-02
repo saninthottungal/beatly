b9f8c3537c131bfa184d7570d029af28c4821b77
sanin <saninthottungalhere@gmail.con>
2024-05-02 11:57:33 +0530

    added theme files

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    primary: Colors.blue.shade400,
+  ),
+);
